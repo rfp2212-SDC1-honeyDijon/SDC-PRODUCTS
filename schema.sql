ff51d62b72d1d18e0bc3bb6569560289cac14e0a
Kathy Ye <kathyye15@gmail.com>
2023-02-17 15:34:43 -0800

    created and loaded data to product table

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE products;
 \c products;
 
 CREATE TABLE products (
-  id PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   campus TEXT,
   name TEXT,
   slogan TEXT,
@@ -15,31 +15,38 @@ CREATE TABLE products (
 );
 
 CREATE TABLE features (
-  id PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   product_id INTEGER REFERENCES products(id),
   name TEXT,
   value TEXT
 );
 
 CREATE TABLE styles (
-  id SERIAL PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
+  product_id INTEGER REFERENCES products(id),
   name TEXT,
-  original_price TEXT,
   sale_price TEXT,
-  is_default BOOLEAN,
-  product_id INTEGER REFERENCES products(id)
+  original_price TEXT,
+  is_default BOOLEAN
 );
 
 CREATE TABLE photos (
-  id SERIAL PRIMARY KEY,
-  thumbnail_url TEXT,
+  id INTEGER PRIMARY KEY,
+  style_id INTEGER REFERENCES styles(id),
   url TEXT,
-  style_id INTEGER REFERENCES styles(id)
+  thumbnail_url TEXT
 );
 
 CREATE TABLE skus (
-  id SERIAL PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   quantity INTEGER,
   size TEXT,
   style_id INTEGER REFERENCES styles(id)
 );
+
+CREATE INDEX products_idx ON products(id, campus, name, slogan, description, category, default_price, created_at, updated_at);
+
+COPY products(id, campus, name, slogan, description, category, default_price, created_at, updated_at)
+  FROM '/Users/kathyye/Desktop/hackreactor/SDC-PRODUCTS/ETL/transformed_data/product.csv'
+  DELIMITER ',' CSV HEADER;
+
