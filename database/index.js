98c6c28a85360e75a9885756324144f79524e340
Kathy Ye <kathyye15@gmail.com>
2023-02-18 14:59:36 -0800

    removed querying role to models in MVC

--- database/index.js
+++ database/index.js
@@ -1,53 +1,6 @@
 const { Pool } = require('pg');
 const postgresqlConfig = require('./config');
 
-const pool = new Pool(postgresqlConfig);
+const db = new Pool(postgresqlConfig);
 
-function getProductsQuery(page, count, callback) {
-  pool.query(`SELECT * FROM products LIMIT ${page * count}`, (err, res) => {
-    if (err) {
-      callback(err);
-    } else {
-      callback(res.rows);
-    }
-  });
-}
-
-function getProductByIdQuery(product_id, callback) {
-  pool.query(`SELECT * FROM products WHERE id = ${product_id}`, (err, res) => {
-    if (err) {
-      callback(err);
-    } else {
-      callback(res.rows);
-    }
-  });
-}
-
-function getRelatedProductQuery(product_id, callback) {
-  pool.query(`SELECT related_product_id FROM related WHERE current_product_id = ${product_id}`, (err, res) => {
-    if (err) {
-      callback(err);
-    } else {
-      callback(res.rows);
-    }
-  });
-}
-
-function getProductStyleQuery(product_id, callback) {
-  pool.query(`SELECT * FROM styles WHERE product_id = ${product_id}`, (err, res) => {
-    if (err) {
-      callback(err);
-    } else {
-      callback(res.rows);
-    }
-  });
-}
-
-pool.end();
-
-module.exports = {
-  getProductsQuery,
-  getProductByIdQuery,
-  getRelatedProductQuery,
-  getProductStyleQuery,
-};
+module.exports = db;
