76c56737ea7d45262576f2ba0bcbe2f504475a03
Kathy Ye <kathyye15@gmail.com>
2023-02-18 18:41:36 -0800

    implemented pagination in getProducts

--- SDCServer/models.js
+++ SDCServer/models.js
@@ -1,9 +1,9 @@
 const db = require('../database');
 
-const getProducts = (count, page) => db.query(`SELECT * FROM products LIMIT ${page * count}`)
+const getProducts = (count, page) => db.query(`SELECT * FROM products LIMIT ${count} OFFSET ${(page - 1) * count}`)
   .then((results) => results.rows)
   .catch((error) => {
-    throw new Error(`Error retrieving getProducts, error message: ${error.message}`);
+    throw new Error(`Error retrieving products, error message: ${error.message}`);
   });
 
 const getProduct = (productId) => db.query(`SELECT * FROM products WHERE id = ${productId}`)
@@ -14,7 +14,7 @@ const getProduct = (productId) => db.query(`SELECT * FROM products WHERE id = ${
     return results.rows[0];
   })
   .catch((error) => {
-    throw new Error(`Error retrieving Product, error message: ${error.message}`);
+    throw new Error(`Error retrieving product ${productId}, error message: ${error.message}`);
   });
 
 module.exports = {
