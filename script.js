5716edbb28552a6d6b45ed8c2887f6705018d529
Kathy Ye <kathyye15@gmail.com>
2023-02-22 00:21:31 -0800

    stress test based on ramping vus

---
+++ script.js
@@ -0,0 +1,25 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  scenarios: {
+    stress: {
+      executor: 'ramping-vus',
+      startVUs: 0,
+      stages: [
+        { duration: '20s', target: 10 }, // below normal load
+        { duration: '10s', target: 0 }, // scale down. Recovery stage.
+      ],
+    },
+  },
+};
+
+export default function () {
+  const BASE_URL = 'http://localhost:8080';
+  const responses = http.batch([
+    ['GET', `${BASE_URL}/products`, null, { timeout: '30s' }],
+    ['GET', `${BASE_URL}/products/40396`, null, { timeout: '30s' }],
+    ['GET', `${BASE_URL}/products/40396/styles`, null, { timeout: '30s' }],
+    ['GET', `${BASE_URL}/products/40396/related`, null, { timeout: '30s' }],
+  ]);
+}
