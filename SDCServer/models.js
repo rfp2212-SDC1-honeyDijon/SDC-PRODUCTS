5b44cce6e5f011e370b63807f2faef75299aa01c
Kathy Ye <kathyye15@gmail.com>
2023-02-23 20:58:26 -0800

    return empty array instead of undefined

--- SDCServer/models.js
+++ SDCServer/models.js
@@ -1,7 +1,7 @@
 const db = require('../database');
 
 const getProducts = (count, page) => db.query(`SELECT * FROM products LIMIT ${count} OFFSET ${(page - 1) * count}`)
-  .then((results) => results.rows)
+  .then((results) => results.rows ?? [])
   .catch((error) => {
     throw new Error(`Error retrieving products, error message: ${error.message}`);
   });
@@ -24,7 +24,7 @@ LEFT JOIN features f ON p.id = f.product_id
 WHERE p.id = ${productId}
 GROUP BY p.id
 `)
-  .then((results) => results.rows[0])
+  .then((results) => results.rows[0] ?? [])
   .catch((error) => {
     throw new Error(`Error retrieving product ${productId}, error message: ${error.message}`);
   });
@@ -65,13 +65,13 @@ FROM styles s
 WHERE s.product_id = ${productId}
 GROUP BY s.product_id;
 `)
-  .then((results) => results.rows[0])
+  .then((results) => results.rows[0] ?? [])
   .catch((error) => {
     throw new Error(`Error retrieving product ${productId}, error message: ${error.message}`);
   });
 
 const getRelated = (productId) => db.query(`SELECT array_agg(related_product_id) FROM related WHERE current_product_id = ${productId}`)
-  .then((results) => results.rows[0].array_agg)
+  .then((results) => results.rows[0].array_agg ?? [])
   .catch((error) => {
     throw new Error(`Error retrieving product ${productId}, error message: ${error.message}`);
   });
