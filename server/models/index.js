9638c4b0936a94813f374655c2a7270c3cae2e09
Kathy Ye <kathyye15@gmail.com>
2023-02-23 21:04:47 -0800

    deleted FEC

--- server/models/index.js
+++
@@ -1,6 +0,0 @@
-module.exports.products = require('./products');
-module.exports.reviews = require('./reviews');
-module.exports.questions = require('./questions');
-module.exports.cart = require('./cart');
-module.exports.interactions = require('./interactions');
-module.exports.thirdParty = require('./thirdParty');
