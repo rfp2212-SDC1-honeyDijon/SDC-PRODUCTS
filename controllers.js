e8bc301a1dd074b7157a25f21aae651b2111dd7e
Kathy Ye <kathyye15@gmail.com>
2023-02-24 17:26:19 -0800

    removed redis implementation, testing results without it first

--- controllers.js
+++ controllers.js
@@ -1,24 +1,11 @@
-const Redis = require('redis');
 const models = require('./models');
-
-const redisClient = Redis.createClient();
-
-redisClient.on('connect', () => console.log('Redis connected!'));
-redisClient.on('error', (err) => console.log('Redis Client Error', err));
-redisClient.connect();
 
 const getProducts = async (req, res) => {
   const count = req.query.count || 5;
   const page = req.query.page || 1;
   try {
-    const cached = await redisClient.get(`products?count=${count}&page=${page}`);
-    if (!cached) {
-      const products = await models.getProducts(count, page);
-      redisClient.set(`products?count=${count}&page=${page}`, JSON.stringify(products));
-      res.send(products);
-      return;
-    }
-    res.send(JSON.parse(cached));
+    const products = await models.getProducts(count, page);
+    res.send(products);
   } catch (e) {
     res.status(400).send(e.message);
   }
@@ -27,14 +14,8 @@ const getProducts = async (req, res) => {
 const getProduct = async (req, res) => {
   const productId = req.params.productid;
   try {
-    const cached = await redisClient.get(`products:${productId}`);
-    if (!cached) {
-      const product = await models.getProduct(productId);
-      redisClient.set(`products:${productId}`, JSON.stringify(product));
-      res.send(product);
-      return;
-    }
-    res.send(JSON.parse(cached));
+    const product = await models.getProduct(productId);
+    res.send(product);
   } catch (e) {
     res.status(400).send(e.message);
   }
@@ -43,14 +24,8 @@ const getProduct = async (req, res) => {
 const getStyles = async (req, res) => {
   const productId = req.params.productid;
   try {
-    const cached = await redisClient.get(`products:${productId}/styles`);
-    if (!cached) {
-      const productStyles = await models.getStyles(productId);
-      redisClient.set(`products:${productId}/styles`, JSON.stringify(productStyles));
-      res.send(productStyles);
-      return;
-    }
-    res.send(JSON.parse(cached));
+    const productStyles = await models.getStyles(productId);
+    res.send(productStyles);
   } catch (e) {
     res.status(400).send(e.message);
   }
@@ -59,14 +34,8 @@ const getStyles = async (req, res) => {
 const getRelated = async (req, res) => {
   const productId = req.params.productid;
   try {
-    const cached = await redisClient.get(`products:${productId}/related`);
-    if (!cached) {
-      const related = await models.getRelated(productId);
-      redisClient.set(`products:${productId}/related`, JSON.stringify(related));
-      res.send(related);
-      return;
-    }
-    res.send(JSON.parse(cached));
+    const related = await models.getRelated(productId);
+    res.send(related);
   } catch (e) {
     res.status(400).send(e.message);
   }
